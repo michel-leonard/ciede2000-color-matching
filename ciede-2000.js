2a44c01592610f05f463d4e187c8137b1f15a73f
Michel Leonard <michel-github-com@sur.ovh>
2025-08-30 04:15:46 +0000

    ΔE2000 consistent across 40+ programming languages.

---
+++ ciede-2000.js
@@ -0,0 +1,76 @@
+// This function written in JavaScript is not affiliated with the CIE (International Commission on Illumination),
+// and is released into the public domain. It is provided "as is" without any warranty, express or implied.
+
+// The classic CIE ΔE2000 implementation, which operates on two L*a*b* colors, and returns their difference.
+// "l" ranges from 0 to 100, while "a" and "b" are unbounded and commonly clamped to the range of -128 to 127.
+function ciede_2000(l_1, a_1, b_1, l_2, a_2, b_2) {
+	"use strict"
+	// Working in JavaScript with the CIEDE2000 color-difference formula.
+	// k_l, k_c, k_h are parametric factors to be adjusted according to
+	// different viewing parameters such as textures, backgrounds...
+	var k_l = 1.0, k_c = 1.0, k_h = 1.0;
+	var n = (Math.sqrt(a_1 * a_1 + b_1 * b_1) + Math.sqrt(a_2 * a_2 + b_2 * b_2)) * 0.5;
+	n = n * n * n * n * n * n * n;
+	// A factor involving chroma raised to the power of 7 designed to make
+	// the influence of chroma on the total color difference more accurate.
+	n = 1.0 + 0.5 * (1.0 - Math.sqrt(n / (n + 6103515625.0)));
+	// Application of the chroma correction factor.
+	var c_1 = Math.sqrt(a_1 * a_1 * n * n + b_1 * b_1);
+	var c_2 = Math.sqrt(a_2 * a_2 * n * n + b_2 * b_2);
+	// atan2 is preferred over atan because it accurately computes the angle of
+	// a point (x, y) in all quadrants, handling the signs of both coordinates.
+	var h_1 = Math.atan2(b_1, a_1 * n), h_2 = Math.atan2(b_2, a_2 * n);
+	h_1 += 2.0 * Math.PI * (h_1 < 0.0);
+	h_2 += 2.0 * Math.PI * (h_2 < 0.0);
+	n = Math.abs(h_2 - h_1);
+	// Cross-implementation consistent rounding.
+	if (Math.PI - 1E-14 < n && n < Math.PI + 1E-14)
+		n = Math.PI;
+	// When the hue angles lie in different quadrants, the straightforward
+	// average can produce a mean that incorrectly suggests a hue angle in
+	// the wrong quadrant, the next lines handle this issue.
+	var h_m = (h_1 + h_2) * 0.5, h_d = (h_2 - h_1) * 0.5;
+	if (Math.PI < n) {
+		h_d += Math.PI;
+		// 📜 Sharma’s formulation doesn’t use the next line, but the one after it,
+		// and these two variants differ by ±0.0003 on the final color differences.
+		h_m += Math.PI;
+		// h_m += h_m < Math.PI ? Math.PI : -Math.PI;
+	}
+	var p = 36.0 * h_m - 55.0 * Math.PI;
+	n = (c_1 + c_2) * 0.5;
+	n = n * n * n * n * n * n * n;
+	// The hue rotation correction term is designed to account for the
+	// non-linear behavior of hue differences in the blue region.
+	var r_t = -2.0 * Math.sqrt(n / (n + 6103515625.0))
+		* Math.sin(Math.PI / 3.0 * Math.exp(p * p / (-25.0 * Math.PI * Math.PI)));
+	n = (l_1 + l_2) * 0.5;
+	n = (n - 50.0) * (n - 50.0);
+	// Lightness.
+	var l = (l_2 - l_1) / (k_l * (1.0 + 0.015 * n / Math.sqrt(20.0 + n)));
+	// These coefficients adjust the impact of different harmonic
+	// components on the hue difference calculation.
+	var t = 1.0	+ 0.24 * Math.sin(2.0 * h_m + Math.PI * 0.5)
+			+ 0.32 * Math.sin(3.0 * h_m + 8.0 * Math.PI / 15.0)
+			- 0.17 * Math.sin(h_m + Math.PI / 3.0)
+			- 0.20 * Math.sin(4.0 * h_m + 3.0 * Math.PI / 20.0);
+	n = c_1 + c_2;
+	// Hue.
+	var h = 2.0 * Math.sqrt(c_1 * c_2) * Math.sin(h_d) / (k_h * (1.0 + 0.0075 * n * t));
+	// Chroma.
+	var c = (c_2 - c_1) / (k_c * (1.0 + 0.0225 * n));
+	// Returning the square root ensures that dE00 accurately reflects the
+	// geometric distance in color space, which can range from 0 to around 185.
+	return Math.sqrt(l * l + h * h + c * c + c * h * r_t);
+}
+
+// GitHub Project : https://github.com/michel-leonard/ciede2000-color-matching
+//   Online Tests : https://michel-leonard.github.io/ciede2000-color-matching
+
+// L1 = 89.6   a1 = 32.9   b1 = -1.7
+// L2 = 89.0   a2 = 38.3   b2 = 2.4
+// CIE ΔE00 = 3.1842649224 (Bruce Lindbloom, Netflix’s VMAF, ...)
+// CIE ΔE00 = 3.1842787486 (Gaurav Sharma, OpenJDK, ...)
+// Deviation between implementations ≈ 1.4e-5
+
+// See the source code comments for easy switching between these two widely used ΔE*00 implementation variants.
